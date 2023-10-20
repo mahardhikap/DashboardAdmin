4f6c9668ecb0c8f0833ec12073732c2faef81eb4
Mahardhika Pratama <putrad578@gmail.com>
2023-10-20 17:56:45 +0700

    feat: add component quick invoice and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import LeftSideBar from './components/LeftSideBar';
 import AllExpenses from './components/AllExpenses';
+import QuickInvoice from './components/QuickInvoice';
 export default function App() {
   return (
-    <div className="container mx-auto bg-[#F7F9FA] h-screen w-screen">
-      <div className="grid grid-cols-5">
+    <div className="container mx-auto bg-[#F7F9FA]">
+      <div className="grid grid-cols-6">
         <div className="col-span-1 bg-white">
           <LeftSideBar />
         </div>
-        <div className="col-span-2 ps-10 pe-5">
+        <div className="col-span-3 ps-10 pe-5">
           <div className="bg-white rounded-lg mt-10">
-            <AllExpenses/>
+            <AllExpenses />
           </div>
-          <div className="bg-white rounded-lg my-5">Quick Invoice</div>
+          <div className="bg-white rounded-lg mt-5 mb-10">
+            <QuickInvoice />
+          </div>
         </div>
         <div className="col-span-2 ps-5 pe-10 mt-10">
-          <div className='bg-white rounded-lg'>My Card</div>
+          <div className="bg-white rounded-lg">My Card</div>
         </div>
       </div>
     </div>
---
+++ src/components/QuickInvoice.jsx
@@ -0,0 +1,80 @@
+const QuickInvoice = () => {
+  return (
+    <div className="p-2">
+      <div className="flex items-center justify-between">
+        <div className="font-medium text-[#064061]">Quick Invoice</div>
+        <div className="text-[#4EB7F2] bg-[#FAFAFA] rounded-full px-4 py-2 font-bold">
+          +
+        </div>
+      </div>
+      <div className="text-[#064061]">Latest transaction</div>
+      <div className="flex items-center gap-2 overflow-x-hidden">
+        <div className="flex items-center gap-3 bg-[#FAFAFA] w-fit p-2 rounded-lg  min-w-[300px]">
+          <div>
+            <img src="/avatar1.svg" />
+          </div>
+          <div>
+            <div className="font-medium text-[#064061]">Madrani Andi</div>
+            <div className="font-small text-xs">Madraniadi20@gmail</div>
+          </div>
+        </div>
+        <div className="flex items-center gap-3 bg-[#FAFAFA] w-fit p-2 rounded-lg  min-w-[300px]">
+          <div>
+            <img src="/avatar2.svg" className="w-[50px]" />
+          </div>
+          <div>
+            <div className="font-medium text-[#064061]">Josua Nunito</div>
+            <div className="font-small text-xs">joshnunito@gmail.com</div>
+          </div>
+        </div>
+        <div className="flex items-center gap-3 bg-[#FAFAFA] w-fit p-2 rounded-lg  min-w-[300px]">
+          <div>
+            <img src="/avatar1.svg" />
+          </div>
+          <div>
+            <div className="font-medium text-[#064061]">Madrani Andi</div>
+            <div className="font-small text-xs">Madraniadi20@gmail</div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2">
+        <div className="ps-2 pe-1">
+          <div className="my-2">
+            <label htmlFor="" className="text-[#064061]">Customer name</label>
+            <input
+              placeholder="Type customer name"
+              className="p-3 w-full rounded-lg bg-[#FAFAFA]"
+            />
+          </div>
+          <div className="my-2">
+            <label htmlFor="" className="text-[#064061]">Item name</label>
+            <input
+              placeholder="Type customer item name"
+              className="p-3 w-full rounded-lg bg-[#FAFAFA]"
+            />
+          </div>
+
+          <div className="p-3 text-[#4EB7F2] font-medium">Add more details</div>
+        </div>
+        <div className="ps-1 pe-2">
+          <div className="my-2">
+            <label htmlFor="" className="text-[#064061]">Customer email</label>
+            <input
+              placeholder="Type customer email"
+              className="p-3 w-full rounded-lg bg-[#FAFAFA]"
+            />
+          </div>
+          <div className="my-2">
+            <label htmlFor="" className="text-[#064061]">Item mount</label>
+            <input className="p-3 w-full rounded-lg bg-[#FAFAFA]" />
+          </div>
+          <button className="p-3 text-center bg-[#4EB7F2] rounded-lg font-medium text-white w-full">
+            Send Money
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuickInvoice;
