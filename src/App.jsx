3d1a77c40a6d3053bf9da1d36e52893508d650fb
Mahardhika Pratama <putrad578@gmail.com>
2023-10-22 18:56:13 +0700

    restyle card and fix issue when change card also change detail

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import QuickInvoice from './components/QuickInvoice';
 import MyCard from './components/MyCard';
 import TransactionHistory from './components/TransactionHistory';
 import { Server } from 'miragejs';
-import data from '../data.json';
 
 let mirageServer;
 
@@ -13,14 +12,107 @@ if (!mirageServer) {
   mirageServer = new Server({
     routes() {
       this.get('api/users', () => {
-        return data;
-      });
+        return [
+          {
+            uniqId: 'ed123-09123-1asdas-12msdna',
+            username: 'Lekan Okeowo',
+            email: 'demo@gmail.com',
+          },
+        ];
+      }),
+        this.get('api/cards/:id', (schema, request) => {
+          const cardId = request.params.id;
+          const cards = [
+            {
+              id: 1,
+              fullname: 'Syah Bandi A',
+              idcard: '0918 8124 0042 8129',
+              logocard: 'https://i.ibb.co/bP8pK49/paypal-removebg-preview.png',
+              expired: '2024-12-01 12:30:45+07',
+              userCard: 'ed123-09123-1asdas-12msdna',
+              latest_transaction: [
+                {
+                  id: 1,
+                  name: 'Madrani Andi',
+                  email: 'madraniadi20@gmail.com',
+                  description: 'Buy domain',
+                  amount: [2150, 'out'],
+                },
+                {
+                  id: 2,
+                  name: 'Josua Nunito',
+                  email: 'joshnunito@gmail.com',
+                  description: 'Buy hosting',
+                  amount: [3521, 'out'],
+                },
+                {
+                  id: 3,
+                  name: 'Google Adsense',
+                  email: 'adsense@google.com',
+                  description: 'Appreciation fee',
+                  amount: [1000, 'in'],
+                },
+              ],
+            },
+            {
+              id: 2,
+              fullname: 'Syah Bandi B',
+              idcard: '8124 0918 8129 0042',
+              logocard: 'https://i.ibb.co/kQgFMv1/Master-Card-Logo.png',
+              expired: '2025-12-01 12:30:45+07',
+              userCard: 'ed123-09123-1asdas-12msdna',
+              latest_transaction: [
+                {
+                  id: 1,
+                  name: 'Web Hosting Services',
+                  email: 'service@hosting.com',
+                  description: 'Buy package server',
+                  amount: [5000, 'out'],
+                },
+                {
+                  id: 2,
+                  name: 'PT. Someone',
+                  email: 'admin@someone.com',
+                  description: 'Web creation services',
+                  amount: [10000, 'in'],
+                },
+                {
+                  id: 3,
+                  name: 'Google Adsense',
+                  email: 'adsense@google.com',
+                  description: 'Appreciation fee',
+                  amount: [500, 'in'],
+                },
+              ],
+            },
+          ];
+          const foundCard = cards.find((c) => c.id === parseInt(cardId));
+          if (foundCard) {
+            return foundCard;
+          } else {
+            return new Response(404, {}, { error: 'Card not found' });
+          }
+        });
     },
   });
 }
 
 export default function App() {
   const [users, setUsers] = useState({});
+  const [card, setCard] = useState(null);
+  const [cardId, setcardId] = useState(1);
+  const [select1, setSelect1] = useState(true);
+  const [select2, setSelect2] = useState(false);
+  const handleOnClick = () => {
+    setSelect1(!select1);
+    setSelect2(!select2);
+  };
+
+  useEffect(() => {
+    fetch(`/api/cards/${cardId}`)
+      .then((response) => response.json())
+      .then((data) => setCard(data));
+  }, [cardId]);
 
   useEffect(() => {
     fetch('api/users')
@@ -31,8 +123,8 @@ export default function App() {
   let totalIncome = 0;
   let totalExpense = 0;
 
-  for (let k = 0; k < users?.latest_transaction?.length; k++) {
-    const transaction = users?.latest_transaction[k].amount;
+  for (let k = 0; k < card?.latest_transaction?.length; k++) {
+    const transaction = card?.latest_transaction[k].amount;
     const [totalCash, type] = transaction;
 
     if (type === 'in') {
@@ -50,17 +142,46 @@ export default function App() {
         </div>
         <div className="col-span-3 px-5 lg:ps-10 lg:pe-5">
           <div className="bg-white rounded-lg mt-10">
-            <AllExpenses balance={users?.balance} income={totalIncome} expenses={totalExpense}/>
+            <AllExpenses
+              balance={totalIncome - totalExpense}
+              income={totalIncome}
+              expenses={totalExpense}
+            />
           </div>
           <div className="bg-white rounded-lg mt-5 mb-10">
-            <QuickInvoice invoice={users?.latest_transaction}/>
+            <QuickInvoice invoice={card?.latest_transaction} />
           </div>
         </div>
         <div className="col-span-2 px-5 lg:ps-5 lg:pe-10 mt-10">
           <div className="bg-white rounded-lg">
-            <MyCard card={users?.card} />
+            <MyCard
+              fullname={card?.fullname}
+              idcard={card?.idcard}
+              expired={card?.expired}
+              logocard={card?.logocard}
+            />
+            <div className="flex p-2 gap-2">
+              <div
+                className={`${
+                  select1 === true ? 'px-5 w-10' : 'px-2 w-1'
+                } py-2  bg-blue-200 rounded-full cursor-pointer`}
+                onClick={() => {
+                  handleOnClick();
+                  setcardId(1)
+                }}
+              ></div>
+              <div
+                className={`${
+                  select2 === true ? 'px-5 w-10' : 'px-2 w-1'
+                } py-2  bg-blue-200 rounded-full cursor-pointer`}
+                onClick={() => {
+                  handleOnClick();
+                  setcardId(2)
+                }}
+              ></div>
+            </div>
             <hr className="border mx-2 my-5" />
-            <TransactionHistory history={users?.latest_transaction}/>
+            <TransactionHistory history={card?.latest_transaction} />
           </div>
         </div>
       </div>
--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,52 +1,36 @@
 import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
 
 const MyCard = (props) => {
   return (
     <div className="p-2">
       <div className="my-3 font-medium text-[#064061]">My Card</div>
-      <Swiper
-        pagination={{
-          dynamicBullets: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {(props.card)?.map((item, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <div className="bg-[#4EB7F2] rounded-lg relative min-w-[345px]">
-                <img src="/mask-group.svg" className="w-full lg:w-auto" />
-                <div className="absolute w-full inset-0">
-                  <div className="flex items-center justify-between px-10 py-3">
-                    <div>
-                      <div className="text-white font-light">Name Card</div>
-                      <div className="text-white font-medium">
-                        {item.fullname}
-                      </div>
-                    </div>
-                    <div>
-                      <img
-                        src={item.logocard}
-                        className="w-[50px] object-cover rounded-lg"
-                      />
-                    </div>
-                  </div>
-                  <div className="px-10 pt-10 sm:pt-40 md:pt-56 lg:pt-10 flex flex-col items-end">
-                    <div className="text-white font-bold">{item.idcard}</div>
-                    <div className="text-white font-light text-sm">
-                    {new Date(`${item.expired}`).toLocaleDateString('id-ID', { year: 'numeric', month: 'numeric', day: 'numeric' })}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+      <div className="bg-[#4EB7F2] rounded-lg relative min-w-[345px]">
+        <img src="/mask-group.svg" className="w-full lg:w-auto" />
+        <div className="absolute w-full inset-0">
+          <div className="flex items-center justify-between px-10 py-3">
+            <div>
+              <div className="text-white font-light">Name Card</div>
+              <div className="text-white font-medium">{props.fullname}</div>
+            </div>
+            <div>
+              <img
+                src={props.logocard}
+                className="w-[50px] object-cover rounded-lg"
+              />
+            </div>
+          </div>
+          <div className="px-10 pt-10 sm:pt-40 md:pt-56 lg:pt-10 flex flex-col items-end">
+            <div className="text-white font-bold">{props.idcard}</div>
+            <div className="text-white font-light text-sm">
+              {new Date(`${props.expired}`).toLocaleDateString('id-ID', {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
